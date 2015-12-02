0edb340747a9cd07209aff0eae5c434a3461e585
mjone33 <mjone33@uwo.ca>
2015-12-02 13:39:19 -0500

    Forgot to save!
    I accidently forgot to save the files with a few changes

--- 3309 Assignment 3/Queries/SELECTS/selectStatements.sql
+++ 3309 Assignment 3/Queries/SELECTS/selectStatements.sql
@@ -1,6 +1,6 @@
-SELECT * FROM medicalStaff WHERE qualification = 'Top 10% in respective field';
+SELECT * FROM medicalStaff WHERE qualification = 'Masters';
 SELECT * FROM familyDoctor WHERE CPSONumber BETWEEN 10000 AND 10010;
-SELECT * FROM medicalStaff WHERE specialization LIKE '%Oct%';
+SELECT * FROM medicalStaff WHERE specialization IN ('Orthopaedics','Cardiology','Emergency') OR specialization LIKE '%Oct%';
 SELECT medicalStaff.fName, medicalStaff.lName 
 FROM medicalStaff 
 JOIN familydoctor ON familydoctor.CPSONumber = medicalstaff.CPSONumber
--- 3309 Assignment 3/Queries/UPDATES/updateScripts.sql
+++ 3309 Assignment 3/Queries/UPDATES/updateScripts.sql
@@ -1,8 +1,8 @@
 DELETE FROM prescription 
-WHERE duration > (4 * (SELECT AVG(duration) FROM prescription));
+WHERE medicalStaffID = (SELECT medicalStaffID FROM medicalstaff WHERE lName LIKE '%Free%' ORDER BY RAND() LIMIT 1);
 UPDATE medicalStaff
 SET hospitalID = (SELECT hospitalID FROM department WHERE departmentName = "Cardiology" ORDER BY RAND() LIMIT 1) 
 WHERE departmentID = (SELECT departmentID FROM department WHERE departmentName="Cardiology" ORDER BY RAND() LIMIT 1);
 UPDATE patient
-SET medicalStaffID = (SELECT medicalStaffID FROM medicalstaff WHERE specialization IN ('Orthopaedics','Maternity','Anaesthetics') ORDER BY RAND() LIMIT 1)
-WHERE patientID = 50;
+SET medicalStaffID = (SELECT employeeID FROM medicalstaff WHERE specialization IN ('Orthopaedics','Maternity','Anaesthetics') ORDER BY RAND() LIMIT 1)
+WHERE patientID = 54;
